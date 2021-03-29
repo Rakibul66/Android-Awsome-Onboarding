6966c001b0664d878e59699fababafcc434c1886
Rakibul66 <roxyhasan76@gmail.com>
2021-03-29 13:29:57 +0600

    first commit

---
+++ app/src/main/java/com/java/onboardinganim/SplassActivity.java
@@ -0,0 +1,41 @@
+package com.java.onboardinganim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.java.onboardinganim.home.HomeActivity;
+import com.java.onboardinganim.onboarding.OnBoardingActivity;
+
+public class SplassActivity extends AppCompatActivity {
+
+    SharedPreferences onBoardingScreen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splass);
+       new Handler().postDelayed(new Runnable() {
+           @Override
+           public void run() {
+                onBoardingScreen = getSharedPreferences("onBoardingScreen",MODE_PRIVATE);
+                boolean isFirstTime = onBoardingScreen.getBoolean("firstTime",true);
+                if (isFirstTime){
+                    SharedPreferences.Editor editor = onBoardingScreen.edit();
+                    editor.putBoolean("firstTime",false);
+                    editor.commit();
+                    Intent intent = new Intent(getApplicationContext(), OnBoardingActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Intent intent1 = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent1);
+                    finish();
+                }
+           }
+       },3000);
+    }
+}
